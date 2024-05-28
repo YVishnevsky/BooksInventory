c78d713b678349b313ae6f8915809fcf08785fd2
yvyshnevskyi <vishnevsky.dev@gmail.com>
2024-05-28 11:45:55 +0300

    set routes explicity

--- BooksInventory.Web/Controllers/BooksController.cs
+++ BooksInventory.Web/Controllers/BooksController.cs
@@ -6,17 +6,24 @@ using System.Web.Http;
 
 namespace BooksInventory.Web.Controllers
 {
+    [RoutePrefix("api/books")]
     public class BooksController(IMediator mediator) : ApiController
     {
         private readonly IMediator _mediator = mediator;
 
         // GET: api/Books
+        [Route("")]
+        [HttpGet]
         public async Task<IEnumerable<BookView>> GetAsync([FromUri] GetBooks getBooksRequest) => await _mediator.Send(getBooksRequest);
 
         // GET: api/Books/5
+        [Route("{id}")]
+        [HttpGet]
         public async Task<BookView> GetAsync(int id) => await _mediator.Send(new GetSingleBook(id));
 
         // POST: api/Books
+        [Route("")]
+        [HttpPost]
         public async Task<IHttpActionResult> PostAsync([FromBody] NewBook book)
         {
             await _mediator.Send(book);
@@ -25,6 +32,8 @@ namespace BooksInventory.Web.Controllers
         }
 
         // PUT: api/Books/5
+        [Route("{id}")]
+        [HttpPut]
         public async Task<IHttpActionResult> PutAsync(int id, [FromBody] UpdateBook book)
         {
             await _mediator.Send(book with { Id = id });
@@ -33,6 +42,8 @@ namespace BooksInventory.Web.Controllers
         }
 
         // DELETE: api/Books/5
+        [Route("{id}")]
+        [HttpDelete]
         public async Task<IHttpActionResult> DeleteAsync(int id)
         {
             await _mediator.Send(new Models.DeleteBook(id));
